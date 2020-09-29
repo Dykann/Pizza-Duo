2ebc9ee575a58682f5d429eb94c06cdc99eacc60
Dykann <dykann3@hotmail.fr>
2020-09-29 23:55:51 +0200

    Start the js code

---
+++ main.js
@@ -0,0 +1,17 @@
+const comms = document.querySelectorAll(".jxjCjc");
+const names = document.querySelectorAll(".TSUbDb");
+const commentary = document.querySelectorAll(".Jtu6Td");
+const reviews = document.querySelectorAll(".PuaHbe");
+const dates = document.querySelectorAll(".dehysf");
+const urlPhoto = document.querySelectorAll(".JrO5Xe");
+
+const commentaires = [...commentary];
+commentaires.map((commentaire) => commentaire.textContent);
+
+// const obj = {
+// name:
+// commentary:
+// review:
+// photo_url:
+// date:
+// }
