fbe9f22000cfdad9b5c0d9ba50367ef0cef1341b
Dykann <dykann3@hotmail.fr>
2020-09-30 17:13:06 +0200

    Get reviews of all customers

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-const comms = document.querySelectorAll(".jxjCjc");
-const names = document.querySelectorAll(".TSUbDb");
-const commentary = document.querySelectorAll(".Jtu6Td");
-const reviews = document.querySelectorAll(".PuaHbe");
-const dates = document.querySelectorAll(".dehysf");
-const urlPhoto = document.querySelectorAll(".JrO5Xe");
+const divReviews = [...document.querySelectorAll(".jxjCjc")];
 
-const commentaires = [...commentary];
-commentaires.map((commentaire) => commentaire.textContent);
-
-// const obj = {
-// name:
-// commentary:
-// review:
-// photo_url:
-// date:
-// }
+divReviews.map((element) => {
+  const customer = {
+    name: element.querySelector("a[href]").textContent,
+    comment: element.querySelector("span[jscontroller=P7L8k]").textContent,
+    review: element.querySelector(".Fam1ne").getAttribute("aria-label"),
+    date: element.querySelector(".dehysf").textContent,
+    photoUrl: element.parentNode.querySelector("img").src,
+  };
+  return customer;
+});
