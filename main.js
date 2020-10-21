5ef9db10444d2ca852c93e7a00f6bd0a41db2156
Dykann <dykann3@hotmail.fr>
2020-10-21 19:40:58 +0200

    Start integrating menu

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-const divReviews = [...document.querySelectorAll(".jxjCjc")];
+// const divReviews = [...document.querySelectorAll(".jxjCjc")];
 
-divReviews.map((element) => {
-  const customer = {
-    name: element.querySelector("a[href]").textContent,
-    comment: element.querySelector("span[jscontroller=P7L8k]").textContent,
-    review: element.querySelector(".Fam1ne").getAttribute("aria-label"),
-    date: element.querySelector(".dehysf").textContent,
-    photoUrl: element.parentNode.querySelector("img").src,
-  };
-  return customer;
-});
+// divReviews.map((element) => {
+//   const customer = {
+//     name: element.querySelector("a[href]").textContent,
+//     comment: element.querySelector("span[jscontroller=P7L8k]").textContent,
+//     review: element.querySelector(".Fam1ne").getAttribute("aria-label"),
+//     date: element.querySelector(".dehysf").textContent,
+//     photoUrl: element.parentNode.querySelector("img").src,
+//   };
+//   return customer;
+// });
+
+// let currentIndexImage = 0;
+// const SPEED = 2000;
+// const carouselImages = [
+//   "public/showcase-pizza.jpg",
+//   "public/showcase-dough.jpg",
+//   "public/showcase-ingredients.jpg",
+// ];
+
+// function carousel() {
+//   document.img.src = carouselImages[currentIndexImage];
+//   if (currentIndexImage < carouselImages.length - 1) {
+//     currentIndexImage++;
+//   } else {
+//     currentIndexImage = 0;
+//   }
+//   setTimeout(function () {
+//     carousel();
+//   }, SPEED);
+// }
+
+// carousel();
+
+// const image = document.querySelector("img");
+
+// window.addEventListener("load", isse);
+
+// function isse() {
+//   image.classList.add("open");
+// }
