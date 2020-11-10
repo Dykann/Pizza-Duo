ff82463f423001b35a939c9b1a7fe8b9c2b6afbc
Tony Khaov <tony.khaov@gmail.com>
2020-11-10 14:20:43 +0100

    correctif js

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ const slideshowImages = document.querySelectorAll(".slideshow-img");
 const SPEED = 6000;
 let currentImagePosition = 0;
 
+// tu dois enlever cette ligne et mettre directement l'opacite a 1 dans le HTML pour la premiere image
+// car dans ce cas là, l'opacité est a 0 (image cachee) et elle attend de loader le javascript pour la faire apparaitre
 slideshowImages[currentImagePosition].style.opacity = 1;
 
 function nextImage() {
@@ -31,6 +33,10 @@ setInterval(nextImage, SPEED);
 
 const reviewsUl = document.querySelector(".reviews");
 
+
+// wrap tout ca dans une function que t'appelleras getGoogleReviews(reviews)
+// ou reviews est un argument qui permettra de changer le nombre de reviews qu'on recupere
+// et tu appelleras getGoogleReviews(5)
 fetch("https://netflixvirus.vercel.app/api/pizzaduo/4")
   .then((res) => res.json())
   .then((data) => {
