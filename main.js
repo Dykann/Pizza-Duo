7a0fca941c2ffb6a15558c4acec61b1b4a517bca
Dykann <dykann3@hotmail.fr>
2020-11-10 22:39:48 +0100

    Make a better code for the js

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-// const divReviews = [...document.querySelectorAll(".jxjCjc")];
-
-// divReviews.map((element) => {
-//   const customer = {
-//     name: element.querySelector("a[href]").textContent,
-//     comment: element.querySelector("span[jscontroller=P7L8k]").textContent,
-//     review: element.querySelector(".Fam1ne").getAttribute("aria-label"),
-//     date: element.querySelector(".dehysf").textContent,
-//     photoUrl: element.parentNode.querySelector("img").src,
-//   };
-//   return customer;
-// });
-
 const slideshowImages = document.querySelectorAll(".slideshow-img");
 
 const SPEED = 6000;
 let currentImagePosition = 0;
-
-slideshowImages[currentImagePosition].style.opacity = 1;
 
 function nextImage() {
   slideshowImages[currentImagePosition].style.opacity = 0;
@@ -31,13 +16,14 @@ setInterval(nextImage, SPEED);
 
 const reviewsUl = document.querySelector(".reviews");
 
-fetch("https://netflixvirus.vercel.app/api/pizzaduo/4")
-  .then((res) => res.json())
-  .then((data) => {
-    const reviewsHTML = data
-      .map((review) => {
-        const rate = review.review.slice(7, 8);
-        return `<li class="review">
+function getGoogleReviews(max = 4) {
+  fetch(`https://netflixvirus.vercel.app/api/pizzaduo/${max}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const reviewsHTML = data
+        .map((review) => {
+          const rate = review.review.slice(7, 8);
+          return `<li class="review">
         <img class="review--avatar"src="${review.photoUrl}">
         <div class="review--content">
         <span class="review--name">${review.name}</span> 
@@ -45,7 +31,11 @@ fetch("https://netflixvirus.vercel.app/api/pizzaduo/4")
         </span><span class="review--date">${review.date}</span><p class="review--comment">${review.comment}</p>
         </div>
         </li>`;
-      })
-      .join("");
-    reviewsUl.innerHTML = reviewsHTML;
-  });
+        })
+        .join("");
+      reviewsUl.innerHTML = reviewsHTML;
+    })
+    .catch((error) => console.error(error));
+}
+
+getGoogleReviews(4);
