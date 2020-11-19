f84018511c758f6e76d7f896627dce3a7519ab7b
Dykann <dykann3@hotmail.fr>
2020-11-19 15:05:52 +0100

    add function getReviews

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// const divReviews = [...document.querySelectorAll(".jxjCjc")];
+
+// divReviews.map((element) => {
+//   const customer = {
+//     name: element.querySelector("a[href]").textContent,
+//     comment: element.querySelector("span[jscontroller=P7L8k]").textContent,
+//     review: element.querySelector(".Fam1ne").getAttribute("aria-label"),
+//     date: element.querySelector(".dehysf").textContent,
+//     photoUrl: element.parentNode.querySelector("img").src,
+//   };
+//   return customer;
+// });
+
 const slideshowImages = document.querySelectorAll(".slideshow-img");
 
 const SPEED = 6000;
